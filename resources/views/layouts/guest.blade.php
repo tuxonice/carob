3c25af8aff6c169cd35c1a89c0eabdc027290b81
Helder Correia <tuxboy@tlab.pt>
2022-03-26 13:08:53 +0000

    Add assets for AdminLTE template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,19 +9,19 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
 </head>
 <body class="hold-transition login-page">
 {{ $slot }}
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="{{ asset('js/adminlte.min.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+// mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+//     require('postcss-import'),
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+// ]);
+
+mix.copyDirectory('resources/plugins/fontawesome-free', 'public/plugins/fontawesome-free');
+mix.copyDirectory('resources/plugins/icheck-bootstrap', 'public/plugins/icheck-bootstrap');
+mix.copy('resources/css/adminlte.min.css', 'public/css/adminlte.min.css');
+
+mix.copyDirectory('resources/plugins/jquery/jquery.min.js', 'public/plugins/jquery/jquery.min.js');
+mix.copy('resources/plugins/bootstrap/js/bootstrap.bundle.min.js', 'public/plugins/bootstrap/js/bootstrap.bundle.min.js');
+mix.copy('resources/js/adminlte.min.js', 'public/js/adminlte.min.js');
+
+
